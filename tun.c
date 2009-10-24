4667683883ea70fa40ff5945cc4541f7e54bd86e
Jens Muecke <jens@nons.de>
2009-10-24 02:40:33 +0200

    Finish check for tun device

--- tun.c
+++ tun.c
@@ -6,11 +6,17 @@
 #include <stdlib.h>
 
 #define EXISTS(pathname)         (access(pathname, F_OK) == 0)
+#define TUN_DEV   "/dev/net/tun"
+#define TUN_PATH  "/dev/net"
 
 int tun_create_dev(){
 	mode_t mask = S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 	int i;
-	if (!EXISTS("net") && mkdir("net", mask)) {
+	if (!EXISTS(TUN_PATH) && mkdir(TUN_PATH, mask)) {
+		return 1;
+	}
+	if (mknod (TUN_DEV, S_IFCHR | S_IRUSR | S_IWUSR, makedev(10, 200))) {
+		printf("Error - unable to create tun device - maybe wrong user\n");
 		return 1;
 	}
 	return 0;
@@ -18,9 +24,8 @@ int tun_create_dev(){
 
 int tun_check_or_create() {
 	FILE *fp;
-	if (!EXISTS("noe")) {
-		fclose(fp);
-		return 0;
+	if (!EXISTS(TUN_DEV)) {
+		return tun_create_dev();
 	}
 	return 0;
 }
