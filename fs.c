607bdccce4e8da01cb1f274e02befc2332e5990f
root <root@lintux.(none)>
2009-10-28 22:04:45 -0700

    fills in the fs_mkdir_p() function.  Adds sys/stat.h and errno.h as includes/dependencies.

--- fs.c
+++ fs.c
@@ -1,9 +1,40 @@
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <errno.h>
 
-int fs_mkdir_p(char *path, mode_t mode) {
-	//spaceholder
-	return 0;
+/* Note the function definition -- this CANNOT take a const char for the path!
+ * Returns 0 on success, -1 on error.  errno should be set.
+ * On success, path is returned intact.  On failure, path is undefined.
+ */
+int fs_mkdir_p( char *path, mode_t mode ) {
+	int err,pos,i;
+
+	err = mkdir( path, mode );
+	if( err && errno == ENOENT ) {
+
+		/* find the last occurance of '/' */
+		for( i = 0, pos = 0; path[ i ] != '\0'; i++ ) {
+			if( path[ i ] == '/' )
+				pos = i;
+		}
+
+		/* return -1 if '/' is not found, or found only at position 1
+		 * ("/" is not a valid mkdir) */
+		if( pos == 0 )
+			return -1;
+
+		path[ pos ] = '\0';
+		err = fs_mkdir_p( path, mode );
+		if( err )
+			return err;
+
+		path[ pos ] = '/';
+		err = mkdir( path, mode );
+	}
+
+	return err;
 }
+
 
 int fs_cp_r(char *src, char *dest) {
 	//spaceholder
