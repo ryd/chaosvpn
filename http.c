a90ee118b51303fd4a4886b5ebae65734bbc9f4e
Sven-Haegar Koch <haegar@sdinet.de>
2009-11-07 00:59:26 +0100

    check result from curl call and output error message

--- http.c
+++ http.c
@@ -22,6 +22,7 @@ int http_request(char *url, struct buffer *response) {
 	CURL *curl;
 	CURLcode res;
 	struct MemoryStruct chunk;
+	char curlerror[CURL_ERROR_SIZE];
 
 	chunk.memory=NULL;
 	chunk.size = 0;
@@ -39,12 +40,18 @@ int http_request(char *url, struct buffer *response) {
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, "ChaosVPNclient/2.0");
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curlerror);
 
 	res = curl_easy_perform(curl);
+
+	if (res) {
+		printf("\nrequest failed: %s\n", curlerror);
+	}
+
 	response->text = chunk.memory;
 
 	curl_easy_cleanup(curl);
 
-	return 0;
+	return res;
 }
 
