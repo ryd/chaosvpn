b50408756c5d236949bce0a4d193acf7953b5d83
Sven-Haegar Koch <haegar@sdinet.de>
2009-11-07 01:07:00 +0100

    set missing CURLOPT_SSL_VERIFYHOST and CURLOPT_SSL_VERIFYPEER curl options, otherwise libcurl-gnutls does not like the www.vpn.hamburg.ccc.de certificate (the openssl libcurl accepts it)

--- http.c
+++ http.c
@@ -41,6 +41,8 @@ int http_request(char *url, struct buffer *response) {
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 	curl_easy_setopt(curl, CURLOPT_USERAGENT, "ChaosVPNclient/2.0");
 	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curlerror);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
 
 	res = curl_easy_perform(curl);
 
