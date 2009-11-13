b25b8fe9809de5477f64faa7435dcbe0f85c05d6
Hans-Christian Esperer <hc@hcesperer.org>
2009-11-13 22:10:36 +0100

    move global settings to settings.h

--- chaosvpn.h
+++ chaosvpn.h
@@ -3,24 +3,5 @@
 
 #define	NOERR	(0)
 
-// Settings
-
-extern char* s_my_peerid;
-extern char* s_my_vpn_ip;
-extern char* s_my_vpn_ip6;
-extern char* s_my_password;
-extern char* s_my_ip;
-extern char* s_networkname;
-extern char* s_tincd_bin;
-extern char* s_ip_bin;
-extern char* s_ifconfig;
-extern char* s_ifconfig6;
-extern char* s_master_url;
-extern char* s_base;
-extern char* s_pidfile;
-extern char* s_interface;
-extern char* s_my_vpn_netmask;
-extern int s_tincd_debuglevel;
-
 
 #endif
---
+++ settings.h
@@ -0,0 +1,24 @@
+#ifndef __SETTINGS_H
+#define __SETTINGS_H
+
+extern char* s_my_peerid;
+extern char* s_my_vpn_ip;
+extern char* s_my_vpn_ip6;
+extern char* s_my_password;
+extern char* s_my_ip;
+extern char* s_networkname;
+extern char* s_tincd_bin;
+extern char* s_ip_bin;
+extern char* s_ifconfig;
+extern char* s_ifconfig6;
+extern char* s_master_url;
+extern char* s_base;
+extern char* s_pidfile;
+extern char* s_interface;
+extern char* s_my_vpn_netmask;
+extern int s_tincd_debuglevel;
+
+int yyparse(void);
+void settings_init_defaults(void);
+
+#endif
