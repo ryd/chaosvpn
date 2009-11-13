26effe697e9e140294b2e1ae7d1a4167f5ed04c0
Hans-Christian Esperer <hc@hcesperer.org>
2009-11-13 12:27:52 +0100

    added remark about default values

---
+++ settings.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+#include "chaosvpn.h"
+
+// TODO: Add possible default values here
+
+char* s_my_peerid = NULL;
+char* s_my_vpn_ip = NULL;
+char* s_my_vpn_ip6 = NULL;
+char* s_my_password = NULL;
+char* s_my_ip = NULL;
+char* s_networkname = NULL;
+char* s_tincd_bin = NULL;
+char* s_ip_bin = NULL;
+char* s_ifconfig = NULL;
+char* s_ifconfig6 = NULL;
+char* s_master_url = NULL;
+char* s_base = NULL;
+char* s_pidfile = NULL;
+int s_tincd_debuglevel = 0;
+
