da268da013647c320b5db22d29336babf217f280
Hans-Christian Esperer <hc@hcesperer.org>
2009-11-22 11:41:03 +0100

    Routine to create pidfile added.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
+#include <fcntl.h>
 
 #include "fs.h"
 #include "list.h"
@@ -199,6 +200,47 @@ int main_create_backup(struct config *config) {
 	return 0;
 }
 
+static int main_create_pidfile() {
+	struct string lockfile;
+	int fh_lockfile;
+	int fh_pidfile;
+	char pidbuf[64];
+	int len;
+	int retval = 0;
+
+	if (s_pidfile == NULL) {
+		fputs("create_pidfile: no PIDFILE is set.\n", stderr);
+		return 2;
+	}
+
+	string_init(&lockfile, 512, 512);
+	string_concat(&lockfile, s_pidfile);
+	string_concat(&lockfile, ".lck");
+    
+	fh_lockfile = open(string_get(&lockfile), O_CREAT | O_EXCL, 0600);
+	if (fh_lockfile == -1) {
+		fputs("create_pidfile: lockfile already exists.\n", stderr);
+		return 111;
+	}
+	close(fh_lockfile);
+
+	// There's a better way to convert an int into a string.
+	len = snprintf(pidbuf, 64, "%d", getpid());
+	fh_pidfile = open(s_pidfile, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (write(fh_pidfile, pidbuf, len) != len) {
+		retval = 1;
+		goto bail_out;
+	}
+
+bail_out:
+	close(fh_pidfile);
+	if (unlink(string_get(&lockfile))) {
+		fprintf(stderr, "create_pidfile: warning: couldn't remove lockfile %s\n", string_get(&lockfile));
+	}
+	return retval;
+}
+
+
 int main (int argc,char *argv[]) {
 	struct config *config = main_initialize_config();
 
@@ -232,10 +274,9 @@ int main (int argc,char *argv[]) {
 			main_write_config_up(config)) {
 		return 1;
 	}
-
-	// TODO pidfile check && tincd start
-
 	(void)puts(".");
+
+	if (main_create_pidfile()) return 1;
 
 	return 0;
 }
