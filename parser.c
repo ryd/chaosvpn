3a562255d7e5d00ce6e0c840ffde2d68225b6ad6
Hans-Christian Esperer <hc@hcesperer.org>
2009-11-29 17:15:57 +0100

    Fix more memory leaks... hopefully ;)

--- parser.c
+++ parser.c
@@ -18,7 +18,7 @@ parser_check_new_item(char *token)
 	if ((*token == '[') && (token[len - 1] == ']')) {
 		name = calloc(sizeof(char), len - 1);
 		memcpy(name, token + 1, len - 2);
-        token[len - 2] = 0;
+		token[len - 2] = 0;
 	}
 	return name;
 }
@@ -37,12 +37,12 @@ parser_check_configitem(char *line, char *config) {
 static struct list_head*
 parser_stringlist(char *item) {
 	struct string_list *i = malloc(sizeof(struct string_list));
-	i->text = item;
+	i->text = strdup(item);
 	return &i->list;
 }
 
 static void
-parser_extent_key(char *line) {
+parser_extend_key(char *line) {
 	my_config->key = realloc(
 			my_config->key, 
 			strlen(my_config->key) + strlen(line) + 2
@@ -64,7 +64,7 @@ parser_create_config(char *name)
 	INIT_LIST_HEAD(&my_config->route_network);
 	INIT_LIST_HEAD(&my_config->route_network6);
 
-	my_config->name = name;
+	my_config->name = strdup(name);
 	my_config->gatewayhost = strdup("");
 	my_config->owner = strdup("");
 	my_config->use_tcp_only = strdup("");
@@ -125,11 +125,13 @@ parser_free_config(struct list_head* configlist)
 
 	list_for_each_safe(i, tmp, configlist) {
 		item = container_of(i, struct peer_config_list, list);
+		printf("Deleting item %s.\n", item->peer_config->name);
 		parser_free_peer_config(item->peer_config);
 		free(item->peer_config);
 		list_del(i);
 		free(item);
 	}
+	
 }
 
 static void
@@ -151,13 +153,15 @@ parser_parse_line(char *line, struct list_head *configlist)
 		i = malloc(sizeof(struct peer_config_list));
 		if (i == NULL) {
 			free(my_config);
+			free(item);
 			return 0;
 		}
 
 		parser_create_config(item);
 		i->peer_config = my_config;
-		list_add_tail((struct list_head *)i, configlist);
-	} else if (my_config) {
+		list_add_tail(&i->list, configlist);
+		free(item);
+	} else if (my_config != NULL) {
 		/* we did not start with a [...] header */
 		/* skip until after first valid header initialized a config section */
 		return 0;
@@ -196,13 +200,13 @@ parser_parse_line(char *line, struct list_head *configlist)
 		strcat(my_config->key, "\n");
 		parse_key_mode = 1;
 	} else if ((item = parser_check_configitem(line, "-----END RSA PUBLIC KEY-----"))) {
-		parser_extent_key(line);
+		parser_extend_key(line);
 		parse_key_mode = 0;
 	} else {
 		if (parse_key_mode) {
-			parser_extent_key(line);
+			parser_extend_key(line);
 		} else {
-			printf("unparsed:%s\n", line);
+			printf("parser: warning: unparsed: %s\n", line);
 		}
 	}
 	return 0;
@@ -212,16 +216,21 @@ int
 parser_parse_config (char *data, struct list_head *config_list) {
 	char *token;
 	char *search = "\n";
+	char *tmp;
 
 	my_config = NULL;
 
-	token = strtok(data, search);
+	data = strdup(data);
+	token = strtok_r(data, search, &tmp);
 	while (token) {
 		if (strncmp(token, "#", 1) &&
-				parser_parse_line(token, config_list)) 
+				parser_parse_line(token, config_list))  {
+			free(data);
 			return 1;
-		token = strtok(NULL, search);
+		}
+		token = strtok_r(NULL, search, &tmp);
 	}
+	free(data);
 
 	return 0;
 }
