9443e2bb9523bdfff6994eb06f0aa4309d0ab478
Hans-Christian Esperer <hc@hcesperer.org>
2009-11-30 11:03:26 +0100

    4096 bytes on the stack is a little heavy; use malloc instead

--- string/string_concat_sprintf.c
+++ string/string_concat_sprintf.c
@@ -8,14 +8,29 @@
 int string_concat_sprintf(struct string* s, const char *msg, ...)
 {
     va_list args;
-    char buffer[4096];
+    char* buffer;
+
+    buffer = malloc(4096);
+    if (!buffer) return 1;
     
     va_start(args, msg);
-    vsnprintf(buffer, sizeof(buffer), msg, args);
+    // TODO: reimplement vsnprintf to directly use string_concatb
+    vsnprintf(buffer, 4096, msg, args);
     va_end(args);
     
-    if (string_concatb(s, buffer, strlen(buffer))) return 1;
-    if (string_concatb(s, "\0", 1)) return 1;
+    if (string_concatb(s, buffer, strlen(buffer))) {
+        goto bail_out;
+        return 1;
+    }
+    if (string_concatb(s, "\0", 1)) {
+        goto bail_out;
+        return 1;
+    }
     s->_u._s.length--;
+    free(buffer);
     return 0;
+
+bail_out:
+    free(buffer);
+    return 1;
 }
