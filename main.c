f9e49d17c2baa8226cb1b06e16820379931ab5de
Sven-Haegar Koch <haegar@sdinet.de>
2010-01-08 05:47:29 +0100

    cleaned up setup of config structure

--- main.c
+++ main.c
@@ -252,32 +252,38 @@ main_init(struct config *config) {
 	yyparse();
 	fclose(yyin);
 
-	// optional params
-	if (s_master_url != NULL)	config->master_url	= s_master_url;
-	if (s_tincd_bin != NULL)	config->tincd_bin	= s_tincd_bin;
+	// first copy all parsed params into config structure
+	if (s_my_peerid != NULL)	config->peerid		= s_my_peerid;
+	if (s_my_vpn_ip != NULL)	config->vpn_ip		= s_my_vpn_ip;
+	if (s_my_vpn_ip6 != NULL)	config->vpn_ip6		= s_my_vpn_ip6;
+	if (s_networkname != NULL)	config->networkname	= s_networkname;
 	if (s_my_ip != NULL)		config->my_ip		= s_my_ip;
+	if (s_tincd_bin != NULL)	config->tincd_bin	= s_tincd_bin;
 	if (s_routeadd != NULL)		config->routeadd	= s_routeadd;
 	if (s_routeadd6 != NULL)	config->routeadd6	= s_routeadd6;
 	if (s_ifconfig != NULL)		config->ifconfig	= s_ifconfig;
 	if (s_ifconfig6 != NULL)	config->ifconfig6	= s_ifconfig6;
+	if (s_master_url != NULL)	config->master_url	= s_master_url;
 	if (s_base != NULL)		config->base_path	= s_base;
 	if (s_pidfile != NULL)		config->pidfile		= s_pidfile;
 
-	//require params
-	if (s_my_peerid == NULL) {
-		fprintf(stderr, "peerid is missing in %s\n", CONFIG_FILE);
-		return 1;
-	}
-	config->peerid = s_my_peerid;
+	// then check required params
+	#define reqparam(paramfield, label) if (config->paramfield == NULL) { \
+		fprintf(stderr, "%s is missing in %s\n", label, CONFIG_FILE); \
+		return 1; \
+		}
 
-	if (s_networkname == NULL) {
-		fprintf(stderr, "networkname is missing in %s\n", CONFIG_FILE);
-		return 1;
-	}
-	config->networkname = s_networkname;
+	reqparam(peerid, "$my_peerid");
+	reqparam(networkname, "$networkname");
+	reqparam(vpn_ip, "$my_vpn_ip");
+	reqparam(routeadd, "$routeadd");
+	reqparam(ifconfig, "$ifconfig");
+	reqparam(base_path, "$base");
 
-	if (stat(s_base, &st) & fs_mkdir_p(s_base, 0700)) {
-		fprintf(stderr, "error: unable to mkdir %s\n", s_base);
+
+	// create base directory
+	if (stat(config->base_path, &st) & fs_mkdir_p(config->base_path, 0700)) {
+		fprintf(stderr, "error: unable to mkdir %s\n", config->base_path);
 		return 1;
 	}
 
