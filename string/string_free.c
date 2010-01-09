e9d6dc951dd56b07974c45d0b8dec1d1e39a0a45
Sven-Haegar Koch <haegar@sdinet.de>
2010-01-10 00:50:01 +0100

    - also reset length to zero after free - now you should be able to re-use a string structure after string_free()   without another string_init()

--- string/string_free.c
+++ string/string_free.c
@@ -3,4 +3,11 @@
 #include "string.h"
 
 void
-string_free(struct string* s){if (s->s) { free(s->s); s->s = NULL; }}
+string_free(struct string* s) {
+	if (s->s) {
+		free(s->s);
+		s->s = NULL;
+		s->_u._s.size = 0;
+		s->_u._s.length = 0;
+	}
+}
