f052700bfac65ac3f17e5d9d499bee6293617f91
Sven-Haegar Koch <haegar@sdinet.de>
2010-01-10 00:46:10 +0100

    fix init in case of malloc error

--- string/string_init.c
+++ string/string_init.c
@@ -7,8 +7,12 @@ string_init(struct string* s, size_t size, size_t growby)
 {
     s->_u._s.length = 0;
     s->s = malloc(size);
-    if (!s->s) { s->_u._s.size = 0; s->_u._s.growby = growby; }
-    s->_u._s.size = size;
-    s->_u._s.growby = growby;
+    if (!s->s) {
+        s->_u._s.size = 0;
+        s->_u._s.growby = growby;
+    } else {
+        s->_u._s.size = size;
+        s->_u._s.growby = growby;
+    }
     return 0;
 }
