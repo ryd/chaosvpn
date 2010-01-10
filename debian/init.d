c5c02de0db74343b7d93e6b480b00f4b86cfa908
Sven-Haegar Koch <haegar@sdinet.de>
2010-01-10 06:15:16 +0100

    reworked debian initscripts
    now they
    - do not throw stupid errormessages anymore even on successfull operation
    - support to setup multiple parallel tinc-networks using the
      chaosvpn client

--- debian/init.d
+++ debian/init.d
@@ -18,10 +18,10 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 DAEMON=/usr/sbin/chaosvpn # Introduce the server's location here
 NAME=chaosvpn             # Introduce the short server's name here
-DESC=chaosvpn             # Introduce a short description here
-LOGDIR=/var/log/chaosvpn  # Log directory to use
+DESC="Agora Link"         # Introduce a short description here
 
-PIDFILE=/var/run/$NAME.pid
+PIDFILE="overridden later"
+
 
 test -x $DAEMON || exit 0
 
@@ -29,14 +29,16 @@ test -x $DAEMON || exit 0
 
 # Default options, these can be overriden by the information
 # at /etc/default/$NAME
-DAEMON_OPTS=""          # Additional options given to the server
+DAEMON_OPTS="-a"        # Additional options given to the server
+# -a == do not run as daemon, onetime fetch+update of config and tincd
+#       restart
 
 DIETIME=10              # Time to wait for the server to die, in seconds
                         # If this value is set too low you might not
                         # let some servers to die gracefully and
                         # 'restart' will not work
 
-#STARTTIME=2             # Time to wait for the server to start, in seconds
+STARTTIME=1             # Time to wait for the server to start, in seconds
                         # If this value is set each time the server is
                         # started (on start or restart) the script will
                         # stall to try to determine if it is running
@@ -45,9 +47,11 @@ DIETIME=10              # Time to wait for the server to die, in seconds
                         # be a false positive (says it did not start
                         # when it actually did)
 
-LOGFILE=$LOGDIR/$NAME.log  # Server logfile
 DAEMONUSER=""           # Users to run the daemons as. If this value
                         # is set start-stop-daemon will chuid the server
+
+CONFIGS="chaosvpn.conf"	# which network config to startup
+
 
 # Include defaults if available
 if [ -f /etc/default/$NAME ] ; then
@@ -76,15 +80,13 @@ if [ -n "$DAEMONUSER" ] ; then
 fi
 
 
-set -e
-
 running_pid() {
 # Check if a given process pid's cmdline matches a given name
-    pid=$1
-    name=$2
+    local pid=$1
+    local name=$2
     [ -z "$pid" ] && return 1
     [ ! -d /proc/$pid ] &&  return 1
-    cmd=`cat /proc/$pid/cmdline | tr "\000" "\n"|head -n 1 |cut -d : -f 1`
+    local cmd=`cat /proc/$pid/cmdline | tr "\000" "\n"|head -n 1 |cut -d : -f 1`
     # Is this the expected server
     [ "$cmd" != "$name" ] &&  return 1
     return 0
@@ -96,15 +98,16 @@ running() {
 
     # No pidfile, probably no daemon present
     [ ! -f "$PIDFILE" ] && return 1
-    pid=`cat $PIDFILE`
+    local pid=`cat $PIDFILE`
     running_pid $pid $DAEMON || return 1
     return 0
 }
 
 start_server() {
+	local errcode
 # Start the process using the wrapper
         if [ -z "$DAEMONUSER" ] ; then
-            start_daemon -p $PIDFILE $DAEMON $DAEMON_OPTS
+            start_daemon -p $PIDFILE $DAEMON $DAEMON_OPTS >/dev/null
             errcode=$?
         else
 # if we are using a daemonuser then change the user id
@@ -116,7 +119,55 @@ start_server() {
         return $errcode
 }
 
+extract_configinfos() {
+	local config="$1"
+	PIDFILE=""
+	DAEMON_OPTS="--help"
+	networkname=""
+	
+	[ -f "/etc/tinc/$config" ] || return 1
+
+	# TODO: replace the following with something smaller like sed/busybox sed
+	networkname=$( perl -ne 's/^.*?\$networkname\s*=\s*\"(.*?)\".*$/$1/ && print $_' <"/etc/tinc/$config" )
+	[ -z "$networkname" ] && return 1
+
+	PIDFILE="/var/run/tinc.$networkname.pid"
+	DAEMON_OPTS="-a -c /etc/tinc/$config"
+	return 0;
+}
+
+start_configs() {
+	local mode="$1"
+	local errcode
+	local config
+	for config in $CONFIGS ; do
+		if [ "$mode" = "reload" ] ; then
+			log_daemon_msg "Reloading $DESC " "$config"
+		else
+			log_daemon_msg "Starting $DESC " "$config"
+		fi
+		errcode=1
+		
+		if extract_configinfos "$config" ; then
+			start_server
+			errcode=$?
+
+			if [ "$errcode" -eq 0 ] ; then
+				[ -n "$STARTTIME" ] && sleep $STARTTIME # Wait some time
+		
+				if [ -f "$PIDFILE" ] ; then
+					# check if tincd is running
+					pidofproc -p "$PIDFILE" /usr/sbin/tincd >/dev/null || errcode=1
+				fi
+			fi
+		fi
+
+		log_end_msg $errcode
+	done
+}
+
 stop_server() {
+	local errcode
 # Stop the process using the wrapper
         if [ -z "$DAEMONUSER" ] ; then
             killproc -p $PIDFILE $DAEMON
@@ -132,6 +183,22 @@ stop_server() {
         return $errcode
 }
 
+stop_configs() {
+	local config
+	for config in $CONFIGS ; do
+		log_daemon_msg "Stopping $DESC " "$config"
+		extract_configinfos "$config"
+		if [ -f "$PIDFILE" ] ; then
+			killproc -p $PIDFILE /usr/sbin/tincd
+			log_end_msg $?
+		else
+			log_progress_msg "apparently not running"
+			log_end_msg 0
+		fi
+	done
+	return 0
+}
+
 reload_server() {
     [ ! -f "$PIDFILE" ] && return 1
     pid=pidofproc $PIDFILE # This is the daemon's pid
@@ -140,130 +207,37 @@ reload_server() {
     return $?
 }
 
-force_stop() {
-# Force the process to die killing it manually
-    [ ! -e "$PIDFILE" ] && return
-    if running ; then
-        kill -15 $pid
-        # Is it really dead?
-        sleep "$DIETIME"s
-        if running ; then
-            kill -9 $pid
-            sleep "$DIETIME"s
-            if running ; then
-                echo "Cannot kill $NAME (pid=$pid)!"
-                exit 0
-            fi
-        fi
-    fi
-    rm -f $PIDFILE
-}
-
 
 case "$1" in
   start)
-        log_daemon_msg "Starting $DESC " "$NAME"
-        # Check if it's running first
-        if running ;  then
-            log_progress_msg "apparently already running"
-            log_end_msg 0
-            exit 0
-        fi
-        if start_server ; then
-            # NOTE: Some servers might die some time after they start,
-            # this code will detect this issue if STARTTIME is set
-            # to a reasonable value
-            [ -n "$STARTTIME" ] && sleep $STARTTIME # Wait some time 
-            if  running ;  then
-                # It's ok, the server started and is running
-                log_end_msg 0
-            else
-                # It is not running after we did start
-                log_end_msg 1
-            fi
-        else
-            # Either we could not start it
-            log_end_msg 1
-        fi
+	start_configs
         ;;
   stop)
-        log_daemon_msg "Stopping $DESC" "$NAME"
-        if running ; then
-            # Only stop the server if we see it running
-            errcode=0
-            stop_server || errcode=$?
-            log_end_msg $errcode
-        else
-            # If it's not running don't do anything
-            log_progress_msg "apparently not running"
-            log_end_msg 0
-            exit 0
-        fi
+	stop_configs
         ;;
   force-stop)
-        # First try to stop gracefully the program
-        $0 stop
-        if running; then
-            # If it's still running try to kill it more forcefully
-            log_daemon_msg "Stopping (force) $DESC" "$NAME"
-            errcode=0
-            force_stop || errcode=$?
-            log_end_msg $errcode
-        fi
+	stop_configs
         ;;
   restart|force-reload)
-        log_daemon_msg "Restarting $DESC" "$NAME"
-        errcode=0
-        stop_server || errcode=$?
-        # Wait some sensible amount, some server need this
-        [ -n "$DIETIME" ] && sleep $DIETIME
-        start_server || errcode=$?
-        [ -n "$STARTTIME" ] && sleep $STARTTIME
-        running || errcode=$?
-        log_end_msg $errcode
+	stop_configs && start_configs
         ;;
-  status)
-
-        log_daemon_msg "Checking status of $DESC" "$NAME"
-        if running ;  then
-            log_progress_msg "running"
-            log_end_msg 0
-        else
-            log_progress_msg "apparently not running"
-            log_end_msg 1
-            exit 0
-        fi
-        ;;
-  # Use this if the daemon cannot reload
+#  status)
+#        log_daemon_msg "Checking status of $DESC" "$NAME"
+#        if running ;  then
+#            log_progress_msg "running"
+#            log_end_msg 0
+#        else
+#            log_progress_msg "apparently not running"
+#            log_end_msg 1
+#            exit 0
+#        fi
+#        ;;
   reload)
-        log_warning_msg "Reloading $NAME daemon: not implemented, as the daemon"
-        log_warning_msg "cannot re-read the config file (use restart)."
+        #log_warning_msg "Reloading $NAME daemon: not implemented, as the daemon"
+        #log_warning_msg "cannot re-read the config file (use restart)."
+        # Reloading is the same as starting for us
+        start_configs "reload"
         ;;
-  # And this if it cann
-  #reload)
-          #
-          # If the daemon can reload its config files on the fly
-          # for example by sending it SIGHUP, do it here.
-          #
-          # If the daemon responds to changes in its config file
-          # directly anyway, make this a do-nothing entry.
-          #
-          # log_daemon_msg "Reloading $DESC configuration files" "$NAME"
-          # if running ; then
-          #    reload_server
-          #    if ! running ;  then
-          # Process died after we tried to reload
-          #       log_progress_msg "died on reload"
-          #       log_end_msg 1
-          #       exit 0
-          #    fi
-          # else
-          #    log_progress_msg "server is not running"
-          #    log_end_msg 1
-          #    exit 0
-          # fi
-                                                                                    #;;
-
   *)
         N=/etc/init.d/$NAME
         echo "Usage: $N {start|stop|force-stop|restart|force-reload|status}" >&2
