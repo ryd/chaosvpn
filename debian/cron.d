fae947d6d2dece1349001f18bc20c9dae62b903d
Sven-Haegar Koch <haegar@sdinet.de>
2010-01-10 06:24:08 +0100

    use "reload", not "restart"

--- debian/cron.d
+++ debian/cron.d
@@ -1,7 +1,7 @@
 #
 # Regular cron jobs for the chaosvpn package
 #
-0 4,11	* * *	root	[ -x /etc/init.d/chaosvpn ] && invoke-rc.d chaosvpn restart
+0 4,11	* * *	root	[ -x /etc/init.d/chaosvpn ] && invoke-rc.d chaosvpn reload
 
 # TODO: do not run at a fixed some, add some random to avoid
 #       all clients hitting the server at the same time
