4f6fe00ded6d9b9b17369add45b14a540f81a201
Hans-Christian Esperer <hc@hcesperer.org>
2010-01-27 20:47:34 +0100

    Add "static string" function

--- string/string.h
+++ string/string.h
@@ -58,6 +58,15 @@ string_ensurez(struct string* s)
     return 0;
 }
 
+static inline void
+string_mkstatic(struct string* s, char* z)
+{
+    s->s = z;
+    s->_u._s.growby = 0;
+    s->_u._s.size = strlen(z);
+    s->_u._s.length = strlen(z);
+}
+
 
 /* FUNCTIONS WORKING WITH PLAIN char* */
 /* ---------------------------------- */
