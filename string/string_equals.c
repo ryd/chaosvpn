38a252ba6d27491a8856331cbbc96415e4c97aad
Hans-Christian Esperer <hc@hcesperer.org>
2010-01-27 01:35:04 +0100

    string_equals should only compare the bytes of a string that are actually used, not merely allocated

--- string/string_equals.c
+++ string/string_equals.c
@@ -14,8 +14,8 @@ string_equals(struct string* s1, struct string* s2)
 
     if (s1 == s2) return 0;
     if (s1->s == s2->s) return 0;
-    if (s1->_u._s.size != s2->_u._s.size) return 1;
+    if (s1->_u._s.length != s2->_u._s.length) return 1;
     
-    for (i = 0; i < s1->_u._s.size; i++) if (s1->s[i] != s2->s[i]) return 1;
+    for (i = 0; i < s1->_u._s.length; i++) if (s1->s[i] != s2->s[i]) return 1;
     return 0;
 }
