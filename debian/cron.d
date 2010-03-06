de2b3ef6d1bff84aebb30ccb123d7090706fb585
Sven-Haegar Koch <haegar@sdinet.de>
2010-03-07 00:35:58 +0100

    call invoke-rc.d with full pathname, may not work otherwise

--- debian/cron.d
+++ debian/cron.d
@@ -1,7 +1,7 @@
 #
 # /etc/cron.d/chaosvpn: crontab entries for the chaosvpn package
 #
-23 4,16	* * *	root	[ -x /etc/init.d/chaosvpn ] && sleep $[ $RANDOM / 36 ] && invoke-rc.d chaosvpn reload
+23 4,16	* * *	root	[ -x /etc/init.d/chaosvpn ] && sleep $[ $RANDOM / 36 ] && /usr/sbin/invoke-rc.d chaosvpn reload >/dev/null
 
 # sleeps a random amount of seconds up to around 15 minutes after
 # the cronjob starts to avoid all clients hitting the server at
