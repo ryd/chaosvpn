2a7e66c4d917e08b530b6ffd8371c43a89087ab3
Jens Muecke <jens@nons.de>
2010-06-26 23:42:32 +0200

    Fix test for httplib
    change http_get call (useragent param)
    Add clean in Makefile

--- httplib/test.c
+++ httplib/test.c
@@ -12,6 +12,7 @@ main(int argc, char** argv)
     struct string buffer;
     struct string inp;
     struct string em;
+    struct string ua;
     int res, ser;
 
     --argc; ++argv;
@@ -20,10 +21,10 @@ main(int argc, char** argv)
 puts(*argv);
 
     string_initfromstringz(&inp, *argv);
+    string_initfromstringz(&ua, "testclient");
     string_lazyinit(&buffer, 8192);
     string_lazyinit(&em, 512);
-    printf("calling geturl: %d\n",
-            res = http_get(&inp, &buffer, 123, &ser, &em));
+    printf("calling geturl: %d\n", res = http_get(&inp, &buffer, 123, &ua, &ser, &em));
     if (res == 0) {
         if (ser == 200) {
             printf("Res: %s\n", buffer.s);
