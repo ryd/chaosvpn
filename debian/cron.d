2bdaaf4926dd380043d523a3942939b8507b2940
Sven-Haegar Koch <haegar@sdinet.de>
2010-11-21 21:32:49 +0100

    chaosvpn (2.03-1) unstable; urgency=low
      * dash does not support $RADOM, which is used in Debian cronjob.
        Package now requires bash instead until I find a better solution.
    
     -- Sven-Haegar Koch <haegar@sdinet.de>  Sun, 21 Nov 2010 21:31:07 +0100

--- debian/cron.d
+++ debian/cron.d
@@ -1,7 +1,15 @@
 #
 # /etc/cron.d/chaosvpn: crontab entries for the chaosvpn package
 #
-23 4,16	* * *	root	[ -x /etc/init.d/chaosvpn ] && sleep $[ $RANDOM / 36 ] && /usr/sbin/invoke-rc.d chaosvpn reload >/dev/null
+
+SHELL=/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+
+# note:
+# the $RANDOM below does not work with dash and needs bash!
+
+23 4,16	* * *	root	[ -x /etc/init.d/chaosvpn ] && sleep $(( $RANDOM \% 900 )) && /usr/sbin/invoke-rc.d chaosvpn reload >/dev/null
 
 # sleeps a random amount of seconds up to around 15 minutes after
 # the cronjob starts to avoid all clients hitting the server at
--- version.h
+++ version.h
@@ -1,1 +1,1 @@
-#define VERSION "2.02"
+#define VERSION "2.03"
