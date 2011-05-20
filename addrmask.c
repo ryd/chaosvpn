da22daa03708e1f37e4a529b1a6d2e3998318aa6
Sven-Haegar Koch <haegar@sdinet.de>
2011-05-21 01:38:14 +0200

    addrmask.c: In function ‘addrmask_to_string’: addrmask.c:186:7: warning: assignment discards ‘const’ qualifier from pointer target type [enabled by default]

--- addrmask.c
+++ addrmask.c
@@ -178,7 +178,7 @@ struct addr_info *addrmask_match(struct addr_info *matches, const char *addr)
 bool addrmask_to_string(struct string *target, struct addr_info *addr)
 {
   char buffer[255];
-  char *res;
+  const char *res;
 
   if (!target || !addr)
     return false;
