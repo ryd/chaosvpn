ee2723b3c5491771914446918d956c1a786b091e
Sven-Haegar Koch <haegar@sdinet.de>
2011-05-24 22:26:45 +0200

    chaosvpn.h:196:50: warning: duplicate const

--- chaosvpn.h
+++ chaosvpn.h
@@ -193,8 +193,8 @@ extern bool daemon_sigchld(struct daemon_info*, unsigned int waitbeforerestart);
 
 
 
-extern int fs_writecontents_safe(const char const*, const char const*, const char const*, const int, const int);
-extern int fs_writecontents(const char const* fn, const char const* cnt, const size_t len, const int mode);
+extern int fs_writecontents_safe(const char*, const char*, const char*, const int, const int);
+extern int fs_writecontents(const char * fn, const char * cnt, const size_t len, const int mode);
 extern int fs_mkdir_p(char *, mode_t);
 extern int fs_cp_r(char*, char*);
 extern int fs_empty_dir(char*);
